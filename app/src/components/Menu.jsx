1cffd7dbd221757bf3ed7d260e634922742c6f7a
Hassan <66774893+GHHassan@users.noreply.github.com>
2023-11-23 16:40:33 +0000

    first vite push

---
+++ app/src/components/Menu.jsx
@@ -0,0 +1,14 @@
+
+
+function Menue(){
+    return(
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Menu</li>
+                <li>Order</li>
+                <li>Location</li>
+            </ul>
+        </div>
+    )
+}
